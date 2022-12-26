8eb0d0aca580eae9b0dc217dc8b14017bde4a831
Quentin Smith <qsmith@pythia.cf>
2022-12-26 00:09:32 +0000

    No Firefox for you ma'am!

--- startup.sh
+++ startup.sh
@@ -5,18 +5,30 @@ if [[ "$OSTYPE" == "win32"* ]]; then
     return 1
 fi
 
-WORKSPACEFOLDER="/workspace/Odyssey/"
+WORKPACEFOLDER="/workspace/Odyssey/"
 
 sudo apt update
 sudo apt upgrade -y
 
-# Gitpod-Specific
+# JIC Installs
 sudo apt install libasound2-dev -y
 sudo apt install libpulse-dev -y
 cargo install cbindgen
 sudo apt install libpango1.0-dev -y
+sudo apt autoremove --purge -y
+
+# Because certain things get removed fsr
+python3 -m pip install --user mercurial
+python3 -m pip install --upgrade pip
 
 
 #Git Cinnabar (Required to build for some damn reason)
-sudo cp $WORKPACEFOLDER/scripts/git-cinnabar $WORKSPACEFOLDER/scripts/git-remote-hg /usr/bin
+sudo cp /workspace/Odyssey/scripts/git-cinnabar /workspace/Odyssey/scripts/git-remote-hg /usr/bin
 sudo chmod +x /usr/bin/git-cinnabar /usr/bin/git-remote-hg
+
+# Mozilla Mach thingies
+cd ..
+./src/configure
+cd src/
+./mach bootstrap
+./mach configure
